d1834834443338bf4e19d14fc0f8947b065b4506
EC2 Default User <ec2-user@ip-172-31-10-171.ap-northeast-1.compute.internal>
2023-08-07 00:54:13 +0000

    [finish]3

---
+++ ruby3/amounts.rb
@@ -0,0 +1,4 @@
+amounts = {"りんご"=>2,"いちご"=>5, "みかん"=>3}
+amounts.each do |fruit, amount|
+  puts "#{fruit}は#{amount}です"
+end
---
+++ ruby3/resson6-1.rb
@@ -0,0 +1,7 @@
+puts "計算を始めます"
+puts "二つの数字を入力してください"
+a = gets.to_i
+b = gets.to_i
+puts "計算結果を出力します"
+puts "#{a}*#{b}=#{a*b}"
+puts "計算結果が出力されました"
